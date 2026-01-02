43d023457a32b84b834a62a5d84e157dc5fd15b0
ahmadarain1925-sudo <ahmadarain1925@gmail.com>
2026-01-02 21:56:54 +0500

    activity1 added

---
+++ congratulation/activity1.py
@@ -0,0 +1,11 @@
+# String variables
+title = "🎉 CONGRATULATIONS 🎉"
+name = "Dear Student"
+message = "You have successfully completed your task."
+wish = "Keep working hard and never stop learning."
+
+# String operations
+full_message = title + "\n\n" + name + ",\n" + message + "\n" + wish
+
+# Print output
+print(full_message)
