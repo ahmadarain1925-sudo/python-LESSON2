c68def6a3867b72d413dfa524d1df11c0b2342b8
ahmadarain1925-sudo <ahmadarain1925@gmail.com>
2026-01-09 20:45:52 +0500

    actvities addedd

---
+++ summer times/activity3.py
@@ -0,0 +1,8 @@
+
+
+month = input("Enter month name: ")
+
+if month == "May" or month == "June" or month == "July":
+    print("It is Summer Time")
+else:
+    print("It is not Summer Time")
---
+++ while loop/activity2.py
@@ -0,0 +1,3 @@
+i=1
+while i<=10:
+ print ("I am a loop")
